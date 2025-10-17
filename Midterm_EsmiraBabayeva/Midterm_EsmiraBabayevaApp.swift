ea8924bfb2cbc7334e87b8c07fc1a606e77cbc6b
esmirajavad <esmirababayeva@my.smccd.edu>
2025-10-16 23:09:14 -0700

    Initial Commit

---
+++ Midterm_EsmiraBabayeva/Midterm_EsmiraBabayevaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_EsmiraBabayevaApp.swift
+//  Midterm_EsmiraBabayeva
+//
+//  Created by Kamal Aliyev on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_EsmiraBabayevaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Midterm_EsmiraBabayevaTests/Midterm_EsmiraBabayevaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_EsmiraBabayevaTests.swift
+//  Midterm_EsmiraBabayevaTests
+//
+//  Created by Kamal Aliyev on 10/16/25.
+//
+
+import Testing
+@testable import Midterm_EsmiraBabayeva
+
+struct Midterm_EsmiraBabayevaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
